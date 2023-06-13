7c583347802c4e3e24cee79f35663188574a47ef
dracunik <u20201a901@upc.edu.pe>
2023-06-13 11:33:07 -0500

    Role DTO

---
+++ src/main/java/app/vercel/gympartner/dtos/RoleDTO.java
@@ -0,0 +1,24 @@
+package app.vercel.gympartner.dtos;
+
+import javax.persistence.Column;
+
+public class RoleDTO {
+    private int idRole;
+    private String name;
+
+    public int getIdRole() {
+        return idRole;
+    }
+
+    public void setIdRole(int idRole) {
+        this.idRole = idRole;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
